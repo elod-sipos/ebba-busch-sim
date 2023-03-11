bc378e9cdafbf68cc804ead7b16e7ea5882b9274
Előd Sipos <127198576+elod-sipos@users.noreply.github.com>
2023-03-11 14:10:35 +0000

    Update follow_script.lua

--- follow_script.lua
+++ follow_script.lua
@@ -2,24 +2,24 @@
 # Add a part in 'ServerStorage' and name it 'WBody'
 
 
-local WBody = game:GetService("ServerStorage").WBody
+local Ebba = game:GetService("ServerStorage").Ebba
 
 local function FollowScript(player)
 	local char = player.Character
 	local root = char.HumanoidRootPart
-	local WWBody = WBody:Clone()
-	WWBody.Parent = char
+	local Ebba1 = Ebba:Clone()
+	Ebba1.Parent = char
 	
-	local bodyPos = Instance.new("BodyPosition", WWBody)
+	local bodyPos = Instance.new("BodyPosition", Ebba1)
 	bodyPos.MaxForce = Vector3.new(4000, 4000, 4000)
-local bodyGyro = Instance.new("BodyGyro", WWBody)
+local bodyGyro = Instance.new("BodyGyro", Ebba1)
 bodyGyro.MaxTorque = Vector3.new(4000, 4000, 4000)
 while true do
-	wait()
-	bodyPos.Position = root.Position + Vector3.new(3, 0.1, 3)
-	bodyGyro.CFrame = root.CFrame
+		wait()
+		bodyPos.Position = root.Position + Vector3.new(3, 0.1, 3)
+		bodyGyro.CFrame = root.CFrame
 	end
-
+	
 end
 
 game.Players.PlayerAdded:Connect(function(player)
